8402c7e907b9b9a624437980bd716826b8d6de0b
karart1981 <karapart1981@gmail.com>
2025-06-17 09:37:58 +0400

    I have created a website where you can download images using unsplash.com/api. When you search for an image, it will bring up a limited number of images (for example, 10 images), after which there will be a button: load more, when you click it, 10 more images will be uploaded.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [query, setQuery] = useState("");
 
   return (
-    <div className="p-4 bg-[url('https://images.unsplash.com/photo-1704072386444-f648652392cc?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZnVsbCUyMHNjcmVlbnxlbnwwfHwwfHx8MA%3D%3D')] w-[100%] min-h-[100vh] bg-center bg-cover bg-no-repeat">
+    <div className="p-4 bg-[url('https://images.unsplash.com/photo-1704072386444-f648652392cc?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZnVsbCUyMHNjcmVlbnxlbnwwfHwwfHx8MA%3D%3D')] w-[100%] min-h-[100vh] bg-center bg-cover bg-no-repeat overflow-hidden">
       <h1 className="text-2xl font-bold text-center mb-4 text-white tracking-[2px]">Unsplash Image Search</h1>
       <SearchBar setQuery={setQuery} className="z-[9999]"/>
       <ImageGallery query={query} className="z-[9999]"/>
